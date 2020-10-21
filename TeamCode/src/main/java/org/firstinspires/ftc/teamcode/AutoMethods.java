4c689a2f5a8d83b78e88aae37d68b43cbdad54f5
austindeng <austin12709@gmail.com>
2020-10-21 18:06:05 -0500

    Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMethods.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class AutoMethods extends LinearOpMode {
+    DcMotor fl;
+    DcMotor fr;
+    DcMotor bl;
+    DcMotor br;
+    public void MoveInch(double speed){
+        fl.setPower(speed);
+        bl.setPower(speed);
+        fr.setPower(-speed);
+        br.setPower(-speed);
+    }
+
+    public void MoveInchEncoder(double speed, double ticks){
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        double ticksDis = ticks;
+        while(bl.getCurrentPosition() < ticks){
+
+            ticksDis -= bl.getCurrentPosition();
+            speed = 1-(1/Math.sqrt(ticksDis));
+            if(speed > 1){
+                speed = 1;
+            }
+            bl.setPower(speed);
+            fl.setPower(speed);
+            br.setPower(-speed);
+            fr.setPower(-speed);
+
+
+        }
+
+
+    }
+    public void StrafeRight(double speed,double ticks ){
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        double ticksDis = ticks;
+        while(bl.getCurrentPosition() < ticks){
+
+            ticksDis -= bl.getCurrentPosition();
+            speed = 1-(1/Math.sqrt(ticksDis));
+            if(speed > 1){
+                speed = 1;
+            }
+            bl.setPower(speed);
+            fl.setPower(-speed);
+            br.setPower(speed);
+            fr.setPower(-speed);
+
+
+        }
+
+
+    }
+
+
+
+
+
+    public void ready() {
+        while (!isStopRequested()){
+            fl = hardwareMap.dcMotor.get("fl");
+            fr = hardwareMap.dcMotor.get("fr");
+            bl = hardwareMap.dcMotor.get("bl");
+            br = hardwareMap.dcMotor.get("br");
+        }
+    }
+
+
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoWobbleGoalV1.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class AutoWobbleGoalV1 extends AutoMethods {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ready();
+
+
+        waitForStart();
+
+        char targetZone;
+
+        while (!isStopRequested() && opModeIsActive()){
+            MoveInchEncoder(1, 1380);//put ticks in
+            telemetry.addData("Running MoveInchEncoder", "complete");
+            telemetry.update();
+            sleep(1000);
+
+            targetZone = 'A'; //delete this
+
+            //Put scan thing here
+
+            StrafeRight(1,1239);//change this!
+
+            //pick up the wobble goal
+
+
+            switch(targetZone) {
+                case 'A':
+                    telemetry.addData("targetZone", "A");
+                    telemetry.update();
+                    MoveInchEncoder(1,1000);
+                    sleep(2000);
+                    //drop wobble goal
+
+
+                    break;
+                case 'B':
+                    telemetry.addData("targetZone", "B");
+                    telemetry.update();
+                    MoveInchEncoder(1,1500);
+                    sleep(3000);
+                    //drop wobble goal
+                    MoveInchEncoder(-1, -256);
+                    break;
+                case 'C':
+                    telemetry.addData("targetZone", "C");
+                    telemetry.update();
+                    MoveInchEncoder(1,2000);
+                    sleep(3500);
+                    //drop wobble goal
+                    MoveInchEncoder(-1, -512);
+                    break;
+
+
+
+            }
+
+
+        }
+        fl.setPower(0);
+        fr.setPower(0);
+        bl.setPower(0);
+        br.setPower(0);
+
+    }
+}
