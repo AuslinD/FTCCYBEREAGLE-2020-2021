89c4dd8625f1c0778e58153b928ae5cce786b6c4
Jason P <56182255+JasonL337@users.noreply.github.com>
2020-10-23 17:20:06 -0500

    Update AutoMethods.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMethods.java
@@ -95,15 +95,28 @@ public class AutoMethods extends LinearOpMode {
 }
 
 
-public class StateMachine {
-
-    public String state;
-
-    public  void ChangeState(String nextState)
+ public  void ChangeState(String nextState)
     {
+        // nextatse is moveforward1
+        // allstates index 0 moveforward2
+        // alstate idex 1 mo
+        allStates.add(nextState);
+        // index 0 of allstate = moveForward
+        for (int i = 0; i < allStates.size(); i++)
+        {
+            if (nextState == allStates.get(i))
+            {
+                //telemetry.addData ("staterepeats", nextState);
+            }
+            else if (i > allStates.size() - 1)
+            {
+                state = nextState;
+            }
+        }
         ///// There will be more stuff here once we progress..
-        state = nextState;
+
     }
+
 
 }
 
